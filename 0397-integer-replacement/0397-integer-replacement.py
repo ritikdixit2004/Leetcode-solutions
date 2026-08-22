f5c5cc71f4a940bb3e9bd159fd3c9bf5652e8071
Ritik Dixit <dixitritik106@gmail.com>
2026-08-23 00:58:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (69.37%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -0,0 +1,17 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        operations = 0
+        
+        while n > 1:
+            if n % 2 == 0:
+                n //= 2
+            elif n == 3:
+                n -= 1
+            elif n % 4 == 1:
+                n -= 1
+            else: # n % 4 == 3
+                n += 1
+                
+            operations += 1
+            
+        return operations
